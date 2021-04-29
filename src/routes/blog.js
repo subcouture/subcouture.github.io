c32510cb077d513f8410e0d5d2ca07b4ad613215
Linzi Zhu <linzi.zhu@hotmail.com>
2021-04-30 09:37:12 +1000

    set up basic routing for blog page

---
+++ src/routes/blog.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/brook', function(req, res, next){
+	res.render('brook', {title: 'Brook Zero Pi Header Soldering' });
+});
+
+module.exports = router;
+
